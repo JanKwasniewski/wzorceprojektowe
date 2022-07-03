ae20fa52f63409216222b12b820cadc1652ea7e0
JanKwasniewski <90791151+JanKwasniewski@users.noreply.github.com>
2022-07-03 15:58:48 +0200

    Add files via upload

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RefactoringGuru.DesignPatterns.Singleton.Conceptual.NonThreadSafe
+{
+    
+    public sealed class Singleton
+    {
+        
+        private Singleton() { }
+
+       
+        private static Singleton ?_instance;
+
+       
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton działa, obie zmienne zwierają tę samą instację.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton nie działa, obie zmienan zawierają inne instację.");
+            }
+        }
+    }
+}
